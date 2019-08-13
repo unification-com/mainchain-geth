817708a00dbdd4f66675ff4fb8ae1cf49381310d
Codegnosis <paul@codegnosis.co.uk>
2019-08-13 12:02:11 +0100

    docker - streamline bootstrap scripts, and remove unneccessary CMD flags

--- docker/assets/bootstrap-sans-delve.sh
+++ docker/assets/bootstrap-sans-delve.sh
@@ -7,23 +7,41 @@ sleep $((RANDOM%5+1))
 
 /usr/local/go/bin/go install github.com/unification-com/mainchain/cmd/und
 
+# Override and remove pristine configuration from parent Dockerfile
 rm -rf /root/.und_mainchain
-/root/.go/bin/und --datadir "/root/.und_mainchain" init /root/.go/src/github.com/unification-com/mainchain/genesis/und-devnet-50009.json
-/root/.go/bin/und --datadir "/root/.und_mainchain" account import --password /root/.accountpassword  /root/.privatekey
+mkdir -p "${DATA_DIR}"/und
 
-mkdir -p /root/.und_mainchain/bootnode_keys
-mkdir -p /root/.und_mainchain/und
-cp /root/.go/src/github.com/unification-com/mainchain/docker/assets/bootnode_keys/*.txt /root/.und_mainchain/bootnode_keys
-cp /root/.go/src/github.com/unification-com/mainchain/docker/assets/bootnode_keys/static-nodes-docker.json /root/.und_mainchain/und/static-nodes.json
+# Set either networkid and load und-devnet-50009.json, or --devnet flag (to auto-load hard-coded Genesis)
+if [[ ${DEVNET_JSON} == 'true' ]]; then
+    # Load und-devnet-50009.json
+    echo "init und CMD with und-devnet-50009.json"
+    /root/.go/bin/und --datadir "${DATA_DIR}" --syncmode=full init /root/.go/src/github.com/unification-com/mainchain/genesis/und-devnet-50009.json
 
-if [[ ${NODE_TYPE} == 'validator' ]]; then
-    NODE_FLAGS='--nodekey=/root/.und_mainchain/bootnode_keys/'"${NODE_KEY}"' --password /root/.accountpassword --mine --gasprice 1 --targetgaslimit 420000000 --etherbase '"${UNLOCK_ADDRESS}"' --unlock '"${UNLOCK_ADDRESS}"
+    # set networkid flag
+    NETFLAG='--networkid '"${NETWORK_ID}"' --datadir '"${DATA_DIR}"
 else
-    NODE_FLAGS='--rpc --rpccorsdomain * --rpcaddr 0.0.0.0 --rpcport 8101 --rpcvhosts * --ws --wsaddr 0.0.0.0 --wsport 8111 --wsorigins *'
+    # Set the --devnet flag to load hard-coded default DevNet Genesis block
+    echo "Use --devnet flag"
+    NETFLAG='--devnet'
+fi
+/root/.go/bin/und account import --datadir "${DATA_DIR}" --password /root/.accountpassword  /root/.privatekey
+
+# Setup static-nodes.json
+cp /root/.go/src/github.com/unification-com/mainchain/docker/assets/bootnode_keys/static-nodes-docker.json "${DATA_DIR}"/und/static-nodes.json
+
+# Generate appropriate und CMD flags for EV/RPC node
+if [[ ${NODE_TYPE} == 'validator' ]]; then
+    # Copy EV's nodekey
+    cp /root/.go/src/github.com/unification-com/mainchain/docker/assets/bootnode_keys/"${NODE_KEY}" "${DATA_DIR}"/und/nodekey
+    NODE_FLAGS='--password /root/.accountpassword --mine --gasprice 1 --targetgaslimit 420000000 --etherbase '"${UNLOCK_ADDRESS}"' --unlock '"${UNLOCK_ADDRESS}"
+else
+    NODE_FLAGS='--rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8101 --rpcvhosts "*" --ws --wsaddr 0.0.0.0 --wsport 8111 --wsorigins "*"'
 fi
 
-APP_FLAGS=' --verbosity '"${LOG_LEVEL}"' --datadir /root/.und_mainchain --identity "'"${IDENTITY}"'" --networkid '"${NETWORK_ID}"' --port '"${LISTEN_PORT}"' --syncmode=full --nodiscover'
+# Generate all necessary flags to run
+APP_FLAGS=' --verbosity '"${LOG_LEVEL}"' --identity "'"${IDENTITY}"'" '${NETFLAG}' --port '"${LISTEN_PORT}"' --syncmode=full --nodiscover'
 
-echo ${APP_FLAGS}
+echo "Generated flags:"
+echo ${APP_FLAGS} ${NODE_FLAGS}
 
 /root/.go/bin/und ${APP_FLAGS} ${NODE_FLAGS}
