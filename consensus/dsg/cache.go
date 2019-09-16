4a95f914f8c4ffbc3a65bb1b9cfc2013bb30908a
Indika Piyasena <indika.piyasena@gmail.com>
2019-09-16 16:55:55 +0700

    external method that wires in constants and unpacks the message

--- consensus/dsg/cache.go
+++ consensus/dsg/cache.go
@@ -16,6 +16,10 @@ func NewCache() *Cache {
 	return cache
 }
 
+func (d *Cache) Insert(msg ValidationMessage) bool {
+	return d.insert(common.ActiveSigners, *msg.Number, msg.BlockHash, *msg.VerifierId, msg.Authorize)
+}
+
 func (d *Cache) insert(totalSigners uint64, blockNumber big.Int, blockHash common.Hash, verifierID big.Int, authorize bool) bool {
 	n := blockNumber.Uint64()
 	v := verifierID.Uint64()
