3eccc47e41817c62a72d1220615dd69b802dda1e
Indika Piyasena <indika.piyasena@gmail.com>
2019-09-18 12:55:10 +0700

    move GetValidatorPool into it's own file because it will grow

--- consensus/dsg/dsg.go
+++ consensus/dsg/dsg.go
@@ -10,7 +10,6 @@ import (
 	"golang.org/x/crypto/sha3"
 	"io"
 	"math/big"
-	"sort"
 )
 
 // BlockProposal represents a block proposal in DSG.
@@ -146,46 +145,6 @@ func EVSlotInternal(blockNumber uint64, blocksInEpoch uint64, numQuarters uint64
 // where the genesis block is block 0, and the current Epoch
 func EVSlot(blockNumber uint64) (uint64, uint64) {
 	return EVSlotInternal(blockNumber, common.BlocksInEpoch, common.NumberOfRounds, common.NumSignersInRound)
-}
-
-// GetValidatorPool is the exported function for getValidatorPool
-func GetValidatorPool() []common.Address {
-	return getValidatorPool()
-}
-
-// getValidatorPool calculates the top staked wallet addresses and returns
-// a list of addresses of size common.NumSignersInEpoch
-func getValidatorPool() []common.Address {
-
-	validatorPool := make([]common.Address, 0)
-
-	// ToDo - get from stateDb, or create snapshot
-	stakedWallets := []struct {
-		Address common.Address
-		Staked  *big.Int
-	}{
-		{common.HexToAddress("0x001A320943d4535e93d31E4A65a6e21C5dF375D7"), big.NewInt(10)},
-		{common.HexToAddress("0x002A956804bAD8DCad148aBFF71515F9B057F7E0"), big.NewInt(1000)},
-		{common.HexToAddress("0x003ADc30A6f4DB59d2698e3D3029fd1BA68b6B15"), big.NewInt(100)},
-		{common.HexToAddress("0x004A435F1D54aA5cc9FCfA0fEB6B8c4a428bbB93"), big.NewInt(1)},
-	}
-
-	sort.Slice(stakedWallets, func(i, j int) bool {
-		cmp := stakedWallets[i].Staked.Cmp(stakedWallets[j].Staked)
-		//reverse order
-		if cmp == 1 {
-			return true
-		}
-		return false
-	})
-
-	top := stakedWallets[:common.NumSignersInEpoch]
-
-	for _, t := range top {
-		validatorPool = append(validatorPool, t.Address)
-	}
-
-	return validatorPool
 }
 
 func VerifierIdFromEtherbase(etherbase common.Address) *big.Int {
