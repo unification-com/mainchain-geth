428f418d06e222b25201787fe62e0427d9451d54
Codegnosis <paul@codegnosis.co.uk>
2019-09-24 16:44:02 +0100

    consensus.dsg - initial EV slot swap function. Reconcile order of hard-coded staked wallets

--- consensus/dsg/signers.go
+++ consensus/dsg/signers.go
@@ -15,9 +15,9 @@ func getStakedWallets() []StakedWallet {
 	// ToDo - get from stateDb, or create snapshot
 
 	stakedWallets := []StakedWallet{
-		{common.HexToAddress("0x001A320943d4535e93d31E4A65a6e21C5dF375D7"), big.NewInt(10)},
-		{common.HexToAddress("0x002A956804bAD8DCad148aBFF71515F9B057F7E0"), big.NewInt(1000)},
-		{common.HexToAddress("0x003ADc30A6f4DB59d2698e3D3029fd1BA68b6B15"), big.NewInt(100)},
+		{common.HexToAddress("0x001A320943d4535e93d31E4A65a6e21C5dF375D7"), big.NewInt(1000)},
+		{common.HexToAddress("0x002A956804bAD8DCad148aBFF71515F9B057F7E0"), big.NewInt(100)},
+		{common.HexToAddress("0x003ADc30A6f4DB59d2698e3D3029fd1BA68b6B15"), big.NewInt(10)},
 		{common.HexToAddress("0x004A435F1D54aA5cc9FCfA0fEB6B8c4a428bbB93"), big.NewInt(1)},
 	}
 	return stakedWallets
@@ -63,4 +63,11 @@ func EVIdFromEtherbase(etherbase common.Address) uint64 {
 	}
 
 	return uint64(0)
+}
+
+// Take the first EV and append it to the end, shifting the slots
+func ShiftEVs(stakedWallets []StakedWallet) []StakedWallet {
+	x, stakedWallets := stakedWallets[0], stakedWallets[1:]
+	stakedWallets = append(stakedWallets, x)
+	return stakedWallets
 }
