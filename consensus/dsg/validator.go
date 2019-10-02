84bcf9ed5bcb574aaae503bfadac6cf3ee238400
Codegnosis <paul@codegnosis.co.uk>
2019-10-02 16:58:54 +0100

    more info in validation log output

--- consensus/dsg/validator.go
+++ consensus/dsg/validator.go
@@ -14,20 +14,16 @@ func (bp *BlockProposal) ValidateBlockProposal(parentHeader *types.Header, cache
 	actualSignerIndex := EVIdFromEtherbase(bp.Proposer)
 
 	if expectedSignerIndex != actualSignerIndex {
-		log.Info("Invalid block proposal - expectedSignerIndex mismatch", "block", bp.Number.Uint64(), "exp", expectedSignerIndex, "got", actualSignerIndex)
+		log.Info("Invalid block proposal - expectedSignerIndex mismatch", "bpnum", bp.Number, "numInv", numTimeouts, "slot", currentSlot, "exp", expectedSignerIndex, "got", actualSignerIndex)
 		return false
-	} else {
-		log.Trace("Proposer OK", "id", expectedSignerIndex)
 	}
 
 	txHash := bp.ProposedBlock.TxHash()
 	txDerivedHash := types.DeriveSha(bp.ProposedBlock.Transactions())
 
 	if txHash != txDerivedHash {
-		log.Info("Invalid block proposal - txHash mismatch", "block", bp.Number.Uint64(), "exp", txDerivedHash, "got", txHash)
+		log.Info("Invalid block proposal - txHash mismatch", "block", bp.Number, "exp", txDerivedHash, "got", txHash)
 		return false
-	} else {
-		log.Trace("txHash OK", "txHash", txHash)
 	}
 
 	// TODO: check against this EV's own block TxHash
